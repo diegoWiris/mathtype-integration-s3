6c9a65ed89bf7f7126cfa0188311b17d9aaa0abc
Diego Lao <diego@wiris.com>
2019-05-13 16:37:43 +0200

    Change thrown errors

--- S3StorageAndCacheExample.php
+++ S3StorageAndCacheExample.php
@@ -80,6 +80,7 @@ class com_wiris_plugin_storage_S3StorageAndCacheExample implements com_wiris_plu
 		}
 		catch (Exception $e) {
 			throw new Exception("Error: can't delete cache: " . Std::string($e->getMessage()));
+			exit(1);
 		}
 	}
 
